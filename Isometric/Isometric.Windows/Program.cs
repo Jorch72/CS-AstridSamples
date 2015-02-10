2441f4ff691eff5fa1c9ee0ec514933d6aa4a1f2
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-02-10 14:28:47 -0800

    Isometric sample to test TextureAtlasGDX

---
+++ Isometric/Isometric.Windows/Program.cs
@@ -0,0 +1,24 @@
+﻿using Astrid.Windows;
+using Isometric;
+namespace Isometric.Windows
+{
+    class Program
+    {
+        static void Main()
+        {
+            var config = new WindowsApplicationConfig
+            {
+                Title = "Isometric Astrid Demo",
+                Width = 800,
+                Height = 540,
+                ContentPath = "Content"
+            };
+
+            using (var application = new WindowsApplication(config))
+            {
+                var game = new Game(application);
+                application.Run(game);
+            }
+        }
+    }
+}
